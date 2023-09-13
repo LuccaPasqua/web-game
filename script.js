6d7697d14bc579165ef48034dfbe8fab2244993c
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2023-09-13 14:52:43 -0500

    commit-2

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+var divDoBotaoNorteElement = document.getElementById('divDoBotaoNorte')
+var divDoBotaoLesteElement = document.getElementById('divDoBotaoLeste')
+var divDoBotaoOesteElement = document.getElementById('divDoBotaoOeste')
+var divDoBotaoSulElement = document.getElementById('divDoBotaoSul')
+
+var vidasDoJogadorElement = document.getElementById('vidasDoJogador')
+
+var botaoPraResposta1 = document.createElement('button')
+var botaoPraResposta2 = document.createElement('button')
+var botaoPraResposta3 = document.createElement('button')
+var botaoPraResposta4 = document.createElement('button')
+
+var flexaNorteElement = document.getElementById('flexaNorte')
+var flexaOesteElement = document.getElementById('flexaOeste')
+var flexaLesteElement = document.getElementById('flexaLeste')
+var flexaSulElement = document.getElementById('flexaSul')
+
+
+
 var containerJogoElement = document.getElementById('containerJogo')
 var espadasSpanElement = document.getElementById('espadasSpan');
 var pocoesSpanElement = document.getElementById('pocaosSpan');
 var moedasSpanElement = document.getElementById('moedasSpan');
-var imgMoeda = document.createElement('img')
+var mapaDeAuxilho = document.getElementById('mapaDeAuxilho');
 
+var imgMoeda = document.createElement('img')
+var imgInimigo = document.createElement('img')
+
+var spanTimerInimigo = document.createElement('span')
+var pConta = document.createElement('p')
+
+var numero1=0
+var numero2=0
 
 var isJogoOn = false;
 var posX = 0;
@@ -25,6 +52,24 @@ var pocoes = 0;
 espadasSpanElement.textContent = `${espadas} / ${espadasNoJogo}`;
 pocoesSpanElement.textContent = `${pocoes} / ${pocoesNoJogo}`;
 moedasSpanElement.textContent = `${moedas} / 16`;
+
+for(let i = 0; i < quantidadeDeCasasTabuleiro; i++){
+  var divDaGridDoMapa = document.createElement('div')
+  divDaGridDoMapa.style.backgroundColor = "red"
+  divDaGridDoMapa.style.width= "100px";
+  divDaGridDoMapa.style.height= "100px";
+
+  mapaDeAuxilho.style="display: grid;grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));grid-gap: 1px;"
+
+  mapaDeAuxilho.appendChild(divDaGridDoMapa);
+
+}
+
+if(vidas< 3 ){
+  if(vidas === 2){
+    vidasDoJogadorElement[0].style = 'filter: grayscale(100%)'
+  }
+}
 
 
 function handleFlechaNorte(){
@@ -186,10 +231,16 @@ function array_chunk(arr, len) {
 // }
 
 function moverPersonagem(posX,posY){
+  numero1 = Math.floor(Math.random() * 100)
+  numero2 = Math.floor(Math.random() * 60)
+  
   localizacaoPersonagem = [posX + tamanhoDoTabuleiro, posY + tamanhoDoTabuleiro]
+  rendenizarBordaDoMapa(posX, posY)
   console.log(containerJogoElement)
   if(containerJogoElement.contains(imgMoeda)){
     containerJogoElement.removeChild(imgMoeda)
+  } else if( containerJogoElement.contains(imgInimigo)){
+    containerJogoElement.removeChild(imgInimigo)
   }
   
   var elementoArrayDaLocalizacao = mapaJogo[localizacaoPersonagem[1]][localizacaoPersonagem[0]]
@@ -209,14 +260,222 @@ function moverPersonagem(posX,posY){
     imgMoeda.style.height= '200px'
 
     setTimeout(()=>{
-      containerJogoElement.removeChild(imgMoeda)
-    },100000000)
+      if(containerJogoElement.contains(imgMoeda)){
+        containerJogoElement.removeChild(imgMoeda)
+      }
+    },2000)
   } else if (elementoArrayDaLocalizacao === '2'){
+    inibirFlexasDeMovimentacao()
+
     console.log('inimigo')
+    imgInimigo.src='./assets/inimigo.webp'
+    containerJogoElement.appendChild(imgInimigo)
+    imgInimigo.style='height:300px; width: 300px'
+    mapaJogo[localizacaoPersonagem[1]][localizacaoPersonagem[0]] = '0'
+    containerJogoElement.appendChild(spanTimerInimigo)
+    
+    var tempoAntesDaMatematica = 5
+    var intervalIID = setInterval(timerAntesDoInimigo, 1000)
+
+    function timerAntesDoInimigo(){
+      spanTimerInimigo.textContent=`${tempoAntesDaMatematica}`
+      if(tempoAntesDaMatematica === 0){
+        jogarJogoDoInimigo()
+        clearInterval(intervalIID)
+      } else {
+        if(tempoAntesDaMatematica % 2 === 0){
+          spanTimerInimigo.style='color: rgb(255, 75, 75)'
+        } else {
+          spanTimerInimigo.style='color: aliceblue'
+        }
+        tempoAntesDaMatematica -= 1
+      }
+    }
+
   } else if (elementoArrayDaLocalizacao === '3'){
     console.log('espada')
   } else {
     console.log('poção')
   }
 
+}
+
+function rendenizarBordaDoMapa(posX, posY){
+  if(posX < tamanhoDoTabuleiro && posX > -tamanhoDoTabuleiro && posY < tamanhoDoTabuleiro && posY > -tamanhoDoTabuleiro){
+    containerJogoElement.style= 'border: 5px solid rgb(61, 7, 7);  width: 350px;height: 350px;'
+  }else if(posX === tamanhoDoTabuleiro){
+    console.log('X é quatro')
+    if(posY == tamanhoDoTabuleiro || posY == -tamanhoDoTabuleiro){
+    console.log('Y é quatro ou menos quatro')
+      if(posY == tamanhoDoTabuleiro){
+        console.log('Y é 4')
+        containerJogoElement.style= 'border-right:40px solid gray; width: 310px;border-top:40px solid gray; height: 310px;'
+      } else if (posY == -tamanhoDoTabuleiro){
+        containerJogoElement.style= 'border-right:40px solid gray; width: 310px;border-bottom:40px solid gray; height: 310px;'
+      }
+    } else {
+      containerJogoElement.style= 'border-right:40px solid gray; width: 310px'
+    }
+  } else if(posX === - tamanhoDoTabuleiro ){
+    console.log('X é - quatro')
+    if(posY == tamanhoDoTabuleiro || posY == -tamanhoDoTabuleiro){
+    console.log('Y é quatro ou menos quatro')
+      if(posY == tamanhoDoTabuleiro){
+        console.log('Y é 4')
+        containerJogoElement.style= 'border-left:40px solid gray; width: 310px;border-top:40px solid gray; height: 310px;'
+      } else if (posY == -tamanhoDoTabuleiro){
+        containerJogoElement.style= 'border-left:40px solid gray; width: 310px;border-bottom:40px solid gray; height: 310px;'
+      }
+    } else {
+      containerJogoElement.style= 'border-left:40px solid gray; width: 310px'
+    }
+  } else if(posY === tamanhoDoTabuleiro ){
+    console.log('Y é - quatro')
+    if(posX < tamanhoDoTabuleiro && posX > -tamanhoDoTabuleiro){
+      containerJogoElement.style= 'border-top:40px solid gray; height: 310px;'
+    } else {
+      containerJogoElement.style= 'border-left:40px solid gray; width: 310px'
+    }
+  } else if(posY === - tamanhoDoTabuleiro ){
+    console.log('Y é - quatro')
+    if(posX < tamanhoDoTabuleiro && posX > -tamanhoDoTabuleiro){
+      containerJogoElement.style= 'border-bottom:40px solid gray; height: 310px;'
+    } else {
+      containerJogoElement.style= 'border-left:40px solid gray; width: 310px'
+    }
+  } 
+}
+
+function jogarJogoDoInimigo(){
+  // var timerParaOInimigoGanhar = 5  
+
+  criarContasMatematicas()
+  //   setInterval(()=> {
+  //     console.log(numero1, numero2)
+  // }, 1000)
+}
+
+
+function inibirFlexasDeMovimentacao(){
+  flexaNorteElement.style='visibility:hidden'
+  flexaOesteElement.style='visibility:hidden'
+  flexaLesteElement.style='visibility:hidden'
+  flexaSulElement.style='visibility:hidden'
+}
+
+function criarContasMatematicas() {
+  flexaNorteElement.style='display:none'
+  flexaOesteElement.style='display:none'
+  flexaLesteElement.style='display:none'
+  flexaSulElement.style='display:none'
+
+  numero1 = Math.floor(Math.random() * 100)
+  numero2 = Math.floor(Math.random() * 60)
+
+  var resultado = numero1 + numero2
+  var indexQueEstaOResultadoCerto = 0
+
+  var resultadoErrado1 = (numero1 + numero2) + Math.floor(Math.random() * 15+1)
+  var resultadoErrado2 = (numero1 + numero2) - Math.floor(Math.random() * 15)
+  var resultadoErrado3 = (numero1 + numero2) - Math.floor(Math.random() * 15)
+
+  var arrayDosResultados = shuffleArray([resultado, resultadoErrado1, resultadoErrado2, resultadoErrado3])
+
+  for(let i = 0; i < 3; i++){
+    if(arrayDosResultados[i] === resultado){
+      indexQueEstaOResultadoCerto = i
+      console.log(indexQueEstaOResultadoCerto)
+    }
+  }
+
+  console.log(arrayDosResultados)
+
+  containerJogoElement.appendChild(pConta)
+  pConta.textContent = `${numero1} + ${numero2}`
+
+  divDoBotaoNorteElement.appendChild(botaoPraResposta1)
+  botaoPraResposta1.textContent = `${arrayDosResultados[0]}`
+  botaoPraResposta1.style='font-size:40px'
+  botaoPraResposta1.onclick = respostaUsuario(arrayDosResultados[0])
+
+  divDoBotaoOesteElement.appendChild(botaoPraResposta2)
+  botaoPraResposta2.textContent = `${arrayDosResultados[1]}`
+  botaoPraResposta2.style='font-size: 40px'
+  botaoPraResposta2.onclick = respostaUsuario(arrayDosResultados[1])
+
+
+
+  divDoBotaoLesteElement.appendChild(botaoPraResposta3)
+  botaoPraResposta3.textContent = `${arrayDosResultados[2]}`
+  botaoPraResposta3.style='font-size: 40px'
+  botaoPraResposta3.onclick = respostaUsuario(arrayDosResultados[2])
+
+
+
+  divDoBotaoSulElement.appendChild(botaoPraResposta4)
+  botaoPraResposta4.textContent = `${arrayDosResultados[3]}`
+  botaoPraResposta4.style='font-size: 40px'
+  botaoPraResposta4.onclick = respostaUsuario(arrayDosResultados[3])
+
+
+  var tempoAntesDaMatematica = 5
+  var intervalIID = setInterval(timerDoInimigo, 1000);
+
+  function timerDoInimigo(){
+
+    spanTimerInimigo.textContent=`${tempoAntesDaMatematica}`
+    if(tempoAntesDaMatematica === 0){
+      clearInterval(intervalIID)
+      perderVida()
+    } else {
+      if(tempoAntesDaMatematica % 2 === 0){
+        spanTimerInimigo.style='color: rgb(255, 75, 75)'
+      } else {
+        spanTimerInimigo.style='color: aliceblue'
+      }
+      tempoAntesDaMatematica -= 1
+    }
+  }
+}
+
+function perderVida(){
+  vidas -= 1
+  if(vidas === 2){
+    vidasDoJogadorElement.children[0].style='filter: grayscale(100%)'
+    voltarAoJogo()
+  } else if(vidas === 1){
+    vidasDoJogadorElement.children[0].style='filter: grayscale(100%)'
+    vidasDoJogadorElement.children[1].style='filter: grayscale(100%)'
+    voltarAoJogo()
+  }
+  if(vidas === 0){
+    gameOver()
+  }
+}
+
+function gameOver(){
+  console.log('Você perdeu')
+}
+
+function respostaUsuario(resposta){
+  if(resposta === numero1 + numero2){
+    moedas += 1
+  }
+}
+
+function voltarAoJogo(){
+  divDoBotaoNorteElement.removeChild(botaoPraResposta1)
+  divDoBotaoOesteElement.removeChild(botaoPraResposta2)
+  divDoBotaoLesteElement.removeChild(botaoPraResposta3)
+  divDoBotaoSulElement.removeChild(botaoPraResposta4)
+
+  flexaNorteElement.style='display:inline; padding:0'
+  flexaOesteElement.style='display:block'
+  flexaLesteElement.style='display:block'
+  flexaSulElement.style='display:block'
+
+  flexaNorteElement.style='visibility:visible'
+  flexaOesteElement.style='visibility:visible'
+  flexaLesteElement.style='visibility:visible'
+  flexaSulElement.style='visibility:visible'
 }
